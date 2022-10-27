55f494a046b157b6a79f562c57b0641a36451cdb
Emperor577 <kholmuhammadov@gmail.com>
2022-10-27 15:18:24 +0500

    changed

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => 'PdDLlVn919JUcpUPBRjlYjwH6m2TYt-f',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
